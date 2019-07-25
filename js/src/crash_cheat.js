b69da020136ca15de7476dcfe0f50b531cf09665
“root <47312374+wwl1664153694@users.noreply.github.com>
2019-07-25 14:57:43 +0800

    Site updated: 2019-07-25 14:57:43

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        $('[rel="icon"]').attr('href', "/images/jiad1.png");
+        document.title = '╭(°A°`)╮ 网站被炸啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        $('[rel="icon"]').attr('href', "/images/favicon-32x32-next.png");
         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
